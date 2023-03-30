9c9cd7ea004585fe0a0ca39df8f9f6d96feee7c2
AnnaHelene01 <91527942+AnnaHelene01@users.noreply.github.com>
2023-03-30 14:19:38 +0200

    fixed warnings

--- src/pages/success/Success.js
+++ src/pages/success/Success.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Header, HeaderLine } from '../../components/styled-components/SiteStyles'
+import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Header, HeaderLine, SubmitBtn } from '../../components/styled-components/SiteStyles'
 import success from './success.png'
 
 const Success = () => {
@@ -7,7 +9,16 @@ const Success = () => {
     <>
       <Header>Order confirmation</Header>
       <HeaderLine></HeaderLine>
-      <img src={success} className='img-fluid shadow-4' alt='...' />
+      <Container>
+        <Row className='m-auto text-center'>
+          <Col sm="6" md="6" lg="6" className='m-auto'>
+            <img src={success} className='img-fluid shadow-4' alt='...' />
+            <Link to='/' className='text-decoration-none text-white' >
+              <SubmitBtn className='mt-3'>Continue Shopping</SubmitBtn>         
+            </Link>
+          </Col>
+        </Row>
+      </Container>
 
     </>
   )
